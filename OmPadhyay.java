0f091a2717ffd2901ab16ed3dbef08e4f76a72e9
Ompadhyay9 <90451128+Ompadhyay9@users.noreply.github.com>
2021-09-14 00:26:38 +0530

    Assignment1- Om Padhyay

---
+++ OmPadhyay.java
@@ -0,0 +1,26 @@
+package om;
+
+public class Om Padhyay {
+
+	public static void main(String[] args) {
+		 System.out.println("Hello, I am Om Padhyay.");
+		 System.out.println("I was born in Ahmedabad,India,and I am 19 years old.");		
+		 System.out.println("I have taken admission in bachelors of computing science as I am interested in it.");
+		 System.out.println("my initials are:-");
+		 System.out.println(" \r\n"
+		 		+ "───────────────────────────────\r\n"
+		 		+ "─██████████████─██████████████─\r\n"
+		 		+ "─██░░░░░░░░░░██─██░░░░░░░░░░██─\r\n"
+		 		+ "─██░░██████░░██─██░░██████░░██─\r\n"
+		 		+ "─██░░██──██░░██─██░░██──██░░██─\r\n"
+		 		+ "─██░░██──██░░██─██░░██████░░██─\r\n"
+		 		+ "─██░░██──██░░██─██░░░░░░░░░░██─\r\n"
+		 		+ "─██░░██──██░░██─██░░██████████─\r\n"
+		 		+ "─██░░██──██░░██─██░░██─────────\r\n"
+		 		+ "─██░░██████░░██─██░░██─────────\r\n"
+		 		+ "─██░░░░░░░░░░██─██░░██─────────\r\n"
+		 		+ "─██████████████─██████─────────\r\n"
+		 		+ "───────────────────────────────         ");
+		 
+
+	
