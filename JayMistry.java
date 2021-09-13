81aa6cf3f0932680b8805764b89a8346227060da
Cipher-935 <90444018+Cipher-935@users.noreply.github.com>
2021-09-13 12:51:38 +0530

    Add files via upload

---
+++ JayMistry.java
@@ -0,0 +1,24 @@
+
+public class JayMistry {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Jay Devika Mistry, you can call me JD.\n");
+		System.out.println("I was born and brought up in Mumbai, India and I am 18 years old.\n");
+		System.out.println("I was always fascinated by videogames right since my childhood and this drew my curiosity towards programming,\nso here I am today with lots of excitement to learn something new and dive deeper into my field of interest.");
+		System.out.println("\nMy initials are:\n\n");
+		System.out.println("       __o__   o__ __o        o          o  \r\n"
+				+ "           |    <|     v\\      <|\\        /|> \r\n"
+				+ "          / \\   / \\     <\\     / \\\\o    o// \\ \r\n"
+				+ "          \\o/   \\o/       \\o   \\o/ v\\  /v \\o/ \r\n"
+				+ "           |     |         |>   |   <\\/>   |  \r\n"
+				+ "          < >   / \\       //   / \\        / \\ \r\n"
+				+ "  \\        |    \\o/      /     \\o/        \\o/ \r\n"
+				+ "   o       o     |      o       |          |  \r\n"
+				+ "   <\\__ __/>    / \\  __/>      / \\        / \\ \r\n"
+				+ "                                              \r\n"
+				+ "                                              \r\n"
+				+ "                                              ");				
+
+	}
+
+}
