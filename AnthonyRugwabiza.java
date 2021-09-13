e3978961d2e3f319da3dcccf0eb7dd4a8ef5563e
AnthonyRugwabiza <90490670+AnthonyRugwabiza@users.noreply.github.com>
2021-09-14 00:31:16 +0200

    Create AnthonyRugwabiza.java

---
+++ AnthonyRugwabiza.java
@@ -0,0 +1,17 @@
+package Assignment_one;
+
+public class AnthonyRugwabiza {
+  public static void main(string[] args){
+    system.out.println("Hello, My name is Anthony Rugwabiza.");
+      system.out.println("\nAnd I am currently pursuing bachelors of computing science at TRU.");
+      system.out.println("\nI choose this course simply becaouse i have alwayse been fascinated by technology and computing.");
+      system.out.println("My Initials are :\n");
+      system.out.println("_   __ __ __   \n" +
+                        "     //\\        _______    \n" +
+                        "    //__\\       ||      \  \n" +
+                        "   //\__/\\      ||  ||  |  \n" +
+                        "  //      \\     ||   ___/  \n" +
+                        " //        \\    ||   \\    \n" +
+                        "//          \\   ||    \\  ");
+  }
+}
