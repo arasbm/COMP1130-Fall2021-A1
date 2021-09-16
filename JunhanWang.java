a2cc57a5e7e17741684a7700e4301959079c5b27
JunhanWang666 <90838437+JunhanWang666@users.noreply.github.com>
2021-09-17 00:04:06 +0800

    JunhanWang.java

---
+++ JunhanWang.java
@@ -0,0 +1,19 @@
+package Assignment_One;
+
+public class JunhanWang {
+    public static void main(String[] args){
+        System.out.println("Hello, everyone.");
+        System.out.println("\nMy name is Junhan Wang.\nI am from Lankao, China.");
+        System.out.println("In China, Bachelor of Computer Science is easier for computer programmers to find jobs.");
+        System.out.println("Studying for a degree in computer science at Thomson River University prepared me for a job.");
+        System.out.println();
+        System.out.println("My Initials are;");
+        System.out.println(" ___      ___  ___     \n" +
+                "   ______    __     __    _       _n" +
+                "  |______|  |  |   |  |  | |     | |n" +
+                "     | |    |  |___|  |  | |  _  | |n" +
+                "     | |    |   ___   |  | | | | | |n" +
+                "  _ _| |    |  |   |  |  |   | |   |n" +
+                " | _ _ /    |__|   |__|  |___| |___|");
+    } 
+}
