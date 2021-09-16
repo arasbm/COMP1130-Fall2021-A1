9876456b93b92cf76cd30270527cca5115bc11b6
JesutofunmiAdebayo <90797210+JesutofunmiAdebayo@users.noreply.github.com>
2021-09-16 03:47:43 +0300

    AdebayoJesutofunmi

---
+++ AdebayoJesutofunmi.java
@@ -0,0 +1,19 @@
+package assignment_one;
+
+public class AdebayoJesutofunmi {
+
+	public static void main(String[] args) {
+	System.out.println("Hello everyone.");
+	System.out.println("My name is Adebayo Jesutofunmi from Kwara State,Nigeria");
+	System.out.println("A fun fact about me is I cannot keep my balance when I laugh so my legs give way and I fall down laughing");
+	System.out.println("I love food, and watching anime gives me joy");
+	System.out.println("For a very long time,I have always been fascinated by how computers work and how technology has brought us here today.");
+	System.out.println("That is why I decided to study computing Science in hopes that I would be able to make a geat impact with it one day.");
+	System.out.println();
+	System.out.println("My initials are");
+	System.out.println("JA");
+			
+    
+	}
+
+}
